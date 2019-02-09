d1374dfcf1bab70fe6a3f0e22119898741fd36b0
stellaxxy <stellaxxy0109@gmail.com>
2019-02-08 22:37:31 -0800

    completed prototype 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function luckyNumber(){
+    let randomNum = Math.floor(Math.random()*1000 + 1);
+    return randomNum;
+}
+
+let user = {
+    name: 'Arial',
+    luckyNumber: luckyNumber()
+};
+
+function Greeting(props){
+    return(
+        <div className="container">
+            <h1>Hello! Welcome {props.user.name}!</h1>
+            <h2 className="text-muted">{props.user.name}'s lucky number is {props.user.luckyNumber}</h2>
+        </div>
+    )
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <Greeting user={user}/>,
     document.getElementById('root')
 );
