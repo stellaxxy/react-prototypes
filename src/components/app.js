5be1b58560685080639393211f242feed5edccb2
stellaxxy <stellaxxy0109@gmail.com>
2019-02-10 14:16:45 -0800

    completed class component prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import Stopwatch from './stopwatch';
 
+const App = () => {
+  return (
+      <div className="container">
+          <h1 className="text-center">Stopwatch</h1>
+          <Stopwatch/>
+      </div>
+  )
+};
 
+export default App;
---
+++ src/components/format_time.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class FormatTime extends Component {
+    constructor(props){
+        super(props);
+        this.timerStyle = {
+            display: 'inline-block',
+            position: 'relative',
+            left: '50%',
+            transform: 'translate(-50%)',
+            width: '383px'
+        }
+    }
+
+    millisecondsToTime(){
+        const {elapsed} = this.props;
+        const sec = elapsed / 1000;
+        const min = sec / 60;
+        return {
+            hour: Math.floor(min / 60),
+            min: Math.floor(min % 60),
+            sec: Math.floor(sec % 60),
+            ms: Math.floor(elapsed % 100)
+        }
+    }
+
+    leadingZero(number){
+        return number < 10 ? `0${number}` : number;
+    }
+
+    trailingZero(number){
+        return number < 10 ? `${number}0` : number;
+    }
+
+    render(){
+        const {hour, min, sec, ms} = this.millisecondsToTime();
+
+        return (
+            <div style={this.timerStyle}>{this.leadingZero(hour)}:{this.leadingZero(min)}:{this.leadingZero(sec)}.{this.trailingZero(ms)}</div>
+        )
+    }
+}
+
+export default FormatTime;
